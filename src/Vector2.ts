379e0aaed067e009dd0c01bb5b30c471448cd0dd
Emil Georgiev <emil.georgiev@delta.bg>
2025-05-20 00:00:31 +0300

    [WIP] Add key listeners, move + camera rotate

--- src/Vector2.ts
+++ src/Vector2.ts
@@ -23,6 +23,26 @@ export default class Vector2 {
         return new Vector2(this.x / that.x, this.y / that.y);
     }
 
+    rotate(deg: number, origin: Vector2): Vector2 {
+        const delta = this.subtract(origin)
+
+        const angle = deg * (Math.PI / 180)
+
+        const cos = Math.cos(angle)
+        const sin = Math.sin(angle)
+
+        const x2 = cos * delta.x - sin * delta.y
+        const y2 = sin * delta.x + cos * delta.y
+
+        return new Vector2(x2, y2).add(origin)
+    }
+
+    getDirection(_moveSpeed: number): Vector2 {
+        // TODO
+
+        return new Vector2(1, 1)
+    }
+
     toArray(): [number, number] {
         return [this.x, this.y];
     }
--- src/main.ts
+++ src/main.ts
@@ -12,10 +12,18 @@ canvas.height = 768
 const GRID_COLS = 16
 const GRID_ROWS = 12
 
-const canvasVector = new Vector2(canvas.width / GRID_COLS, canvas.height / GRID_ROWS)
+// const canvasVector = new Vector2(canvas.width / GRID_COLS, canvas.height / GRID_ROWS)
+
+const framesCap = 60
+const frameInterval = 1000 / framesCap
+const moveSpeed = 0.1 // coordinates per frame
+const camRotateSpeed = 1 // degrees per frame
 
 const cellWidth = canvas.width / GRID_COLS
 const cellHeight = canvas.height / GRID_ROWS
+
+let playerPos = new Vector2(8, 6)
+let directionPos = new Vector2(8, 5)
 
 function drawLine(start: Vector2, end: Vector2, color?: string): void {
   if (!ctx) throw new Error('No canvas context!')
@@ -69,25 +77,88 @@ function drawDot(origin: Vector2, color: string) {
   ctx.fillStyle = color
 
   ctx.beginPath()
-  ctx.arc(x * cellWidth, y * cellHeight, 15, 0, 2 * Math.PI)
+  ctx.arc(x * cellWidth, y * cellHeight, 10, 0, 2 * Math.PI)
   ctx.fill()
   ctx.closePath()
 
 
 }
 
-const playerPos = new Vector2(8, 6)
-
-drawGrid()
-drawDot(playerPos, 'red')
-
-canvas.onmousemove = (e: MouseEvent) => {
-  const mousePos = new Vector2(e.offsetX, e.offsetY).divide(canvasVector)
-
+setInterval(() => {
   drawGrid()
-
-  drawLine(playerPos, mousePos, 'red')
-
-  drawDot(mousePos, 'black')
+  drawLine(playerPos, directionPos, 'red')
+  drawDot(directionPos, 'black')
   drawDot(playerPos, 'red')
+}, frameInterval)
+
+type KeypressInterval = {
+  key: string,
+  id: number,
 }
+
+const keypressIntervals: KeypressInterval[] = []
+
+function initCameraRotateInterval(key: string, deg: number): void {
+  // if such an interval is present, skip
+  if (keypressIntervals.find((i) => i.key === key)) return
+
+  const id = setInterval(() => {
+    directionPos = directionPos.rotate(deg, playerPos)
+  }, frameInterval)
+
+  keypressIntervals.push({ key, id })
+}
+
+function initMoveInterval(key: string, dir: Vector2): void {
+  // if such an interval is present, skip
+  if (keypressIntervals.find((i) => i.key === key)) return
+
+  const id = setInterval(() => {
+    playerPos = playerPos.add(dir)
+  }, frameInterval)
+
+  keypressIntervals.push({ key, id })
+}
+
+function removeKeypressInterval(key: string): void {
+  const idx = keypressIntervals.findIndex((i: KeypressInterval) => i.key === key)
+
+  if (idx === -1) return
+
+  clearInterval(keypressIntervals[idx].id)
+  keypressIntervals.splice(idx, 1)
+}
+
+// listeners for the keypresses
+document.addEventListener('keydown', (event: KeyboardEvent) => {
+  switch (event.key) {
+    case 'ArrowUp':
+      initMoveInterval(event.key, new Vector2(0, -moveSpeed))
+      break
+    case 'ArrowLeft':
+      initCameraRotateInterval(event.key, -camRotateSpeed)
+      break
+    case 'ArrowDown':
+      initMoveInterval(event.key, new Vector2(0, moveSpeed))
+      break
+    case 'ArrowRight':
+      initCameraRotateInterval(event.key, camRotateSpeed)
+      break
+    default:
+      break
+  }
+})
+
+document.addEventListener('keyup', (event: KeyboardEvent) => {
+  removeKeypressInterval(event.key)
+})
+
+// TODO
+// - find ray directions
+// - render walls (according to map)
+// - ray/wall collision
+// - camera + fov + rendering
+// - movement/mouselook
+// - textures?
+// - sound?
+// - framerate counter?
